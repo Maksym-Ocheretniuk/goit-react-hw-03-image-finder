a9bff7c66517156f2adf69f81d2d2e8a7e02c90c
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-07-19 13:21:11 +0300

    upd PropTypes

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
 import { toast } from 'react-toastify';
 
@@ -44,4 +44,6 @@ export class Searchbar extends Component {
   }
 }
 
-// Searchbar.propTypes = {};
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
